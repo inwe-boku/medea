139871a0c8dd66002e1f46e9d2604a732f295fd5
Sebastian Wehrle <seb.wehrle@gmail.com>
2020-05-03 23:43:51 +0200

    updates asparagus settings_asparagus.py

--- projects/asparagus/doc/plots_asparagus.py
+++ projects/asparagus/doc/plots_asparagus.py
@@ -9,7 +9,7 @@ import config as cfg
 
 # %% ----- ----- ----- ----- settings ----- ----- ----- -----
 idx = pd.IndexSlice
-PRICE_CO2 = 60
+PRICE_CO2 = 30
 REFUEL_COLORS = ['#c72321', '#0d8085', '#f0c220', '#595959', '#3b68f9', '#7794dd']
 RES_COLORS = ['#d69602', '#ffd53d', '#3758ba', '#7794dd']
 RES_COLORS3 = ['#d69602', '#e5b710', '#ffd53d', '#3758ba', '#3b68f9', '#7794dd']
@@ -18,7 +18,7 @@ ANNUITY_FACTOR = 0.05827816
 FLH_PV = 857.4938
 FLH_WINDON = 2015.0359
 
-RPATH = os.path.join(cfg.MEDEA_ROOT_DIR, 'projects', 'asparagus', 'results', 'results_200501.csv')
+RPATH = os.path.join(cfg.MEDEA_ROOT_DIR, 'projects', 'asparagus', 'results', 'results.csv')
 FPATH = os.path.join(cfg.MEDEA_ROOT_DIR, 'projects', 'asparagus', 'doc', 'figures')
 
 if not os.path.exists(FPATH):
@@ -134,17 +134,17 @@ results = results.unstack(-1)
 # net electricity generation, net electricity exports, fossil thermal generation, CO2 emissions from energy generation
 
 # prepare data
-restrict_sysops = results.loc[idx['base', PRICE_CO2, :, 36715], idx['AT', ['gen_el', 'gen_biomass', 'gen_storages',
-                                                                           'in_storages', 'gen_renew', 'co2emission']]]
-restrict_sysops[('AT', 'exports')] = results.loc[idx['base', PRICE_CO2, :, 36715], idx['DE', 'exports']]
+restrict_sysops = results.loc[idx['base', PRICE_CO2, :, 36715], idx['AT', ['AnnG_el', 'AnnGBiomass', 'AnnSOut',
+                                                                           'AnnSIn', 'AnnR', 'AnnCO2Emissions']]]
+restrict_sysops[('AT', 'AnnX')] = results.loc[idx['base', PRICE_CO2, :, 36715], idx['DE', 'AnnX']]
 restrict_sysops.index = restrict_sysops.index.droplevel([0, 1, 3])
 restrict_sysops.columns = restrict_sysops.columns.droplevel(0)
-restrict_sysops['Net electricity generation'] = (restrict_sysops[['gen_el', 'gen_storages', 'gen_renew']].sum(axis=1)
-                                                 - restrict_sysops['in_storages'])
-restrict_sysops['Fossil thermal generation'] = restrict_sysops['gen_el'] - restrict_sysops['gen_biomass']
-restrict_sysops = restrict_sysops.drop(['gen_biomass', 'gen_el', 'gen_renew', 'gen_storages', 'in_storages'], axis=1)
-restrict_sysops = restrict_sysops.rename(columns={'co2emission': 'CO2 emissions from energy generation',
-                                                  'exports': 'Net electricity exports'})
+restrict_sysops['Net electricity generation'] = (restrict_sysops[['AnnG_el', 'AnnSOut', 'AnnR']].sum(axis=1)
+                                                 - restrict_sysops['AnnSIn'])
+restrict_sysops['Fossil thermal generation'] = restrict_sysops['AnnG_el'] - restrict_sysops['AnnGBiomass']
+restrict_sysops = restrict_sysops.drop(['AnnGBiomass', 'AnnG_el', 'AnnR', 'AnnSOut', 'AnnSIn'], axis=1)
+restrict_sysops = restrict_sysops.rename(columns={'AnnCO2Emissions': 'CO2 emissions from energy generation',
+                                                  'AnnX': 'Net electricity exports'})
 restrict_sysops = restrict_sysops[['Net electricity generation', 'Net electricity exports',
                                    'Fossil thermal generation', 'CO2 emissions from energy generation']]
 
@@ -160,14 +160,14 @@ plot_lines(restrict_sysops['Fossil thermal generation'] / 1000, os.path.join(FPA
 
 # %% ----- ----- ----- plot wind restriction: changes in cost ----- ----- -----
 # system cost, electricity trade balance, system cost net of trade, cost of air pollution (SOx, NOx, PM)
-undisturbed_cost = results.loc[idx['base', :, :, 36715], idx['AT', ['syscost', 'export_value', 'import_value',
+undisturbed_cost = results.loc[idx['base', :, :, 36715], idx['AT', ['cost_zonal', 'AnnValueX', 'AnnValueI',
                                                                     'cost_airpol']]]
 undisturbed_cost.index = undisturbed_cost.index.droplevel([0, 3])
 undisturbed_cost.columns = undisturbed_cost.columns.droplevel(0)
-undisturbed_cost['trade_balance'] = undisturbed_cost[['export_value', 'import_value']].sum(axis=1)
-undisturbed_cost['syscost_net'] = undisturbed_cost['syscost'] - undisturbed_cost['trade_balance']
-undisturbed_cost = undisturbed_cost.drop(['export_value', 'import_value'], axis=1)
-undisturbed_cost = undisturbed_cost.rename(columns={'syscost': 'System Cost',
+undisturbed_cost['trade_balance'] = undisturbed_cost[['AnnValueX', 'AnnValueI']].sum(axis=1)
+undisturbed_cost['syscost_net'] = undisturbed_cost['cost_zonal'] - undisturbed_cost['trade_balance']
+undisturbed_cost = undisturbed_cost.drop(['AnnValueX', 'AnnValueI'], axis=1)
+undisturbed_cost = undisturbed_cost.rename(columns={'cost_zonal': 'System Cost',
                                                     'trade_balance': 'Electricity trade balance',
                                                     'syscost_net': 'System cost net of trade',
                                                     'cost_airpol': 'Cost of air pollution (SOx, NOx, PM)'})
@@ -191,7 +191,7 @@ plot_lines(restrict_cost.loc[:, 'Cost of air pollution (SOx, NOx, PM)'] / 1000,
 # %% line plot of cost of undisturbed landscapes - baseline capital cost of pv
 undisturbed_base = undisturbed_cost[['System cost net of trade',
                                      'Cost of air pollution (SOx, NOx, PM)']].sum(axis=1).unstack(0)
-wind_add = results.loc[idx['base', :, :, 36715], idx['AT', 'add_wind_on']].unstack(1)
+wind_add = results.loc[idx['base', :, :, 36715], idx['AT', 'add_r_wind_on']].unstack(1)
 wind_add.index = wind_add.index.droplevel([0, 2])
 
 undisturbed_marginal = undisturbed_base.iloc[::-1].diff().divide(wind_add.iloc[::-1].diff().round(8), axis=0) * -1
@@ -202,7 +202,7 @@ plot_lines(undisturbed_marginal / 1000, os.path.join(FPATH, 'undisturbed_base.pd
            xlabel='Added Capacity of Wind [GW]', ylabel='thousand € / MW', color=REFUEL_COLORS)
 
 # %% ------ ----- ----- plot opportunity cost of wind with SHARE of deployment
-uplim = results.loc[idx['base', PRICE_CO2, :, 36715], idx['AT', 'add_wind_on']].max()
+uplim = results.loc[idx['base', PRICE_CO2, :, 36715], idx['AT', 'add_r_wind_on']].max()
 # divide index by uplim, delete indices larger than one except for the smallest and set the smallest to 1
 
 lgnd = []
@@ -229,15 +229,15 @@ plt.savefig(os.path.join(FPATH, 'undisturbed_base_share.pdf'))
 plt.close()
 
 # %% line plot of cost of undisturbed landscapes - low capital cost of pv
-undisturbed_cost_low = results.loc[idx['base', :, :, 16715], idx['AT', ['syscost', 'export_value', 'import_value',
+undisturbed_cost_low = results.loc[idx['base', :, :, 16715], idx['AT', ['cost_zonal', 'AnnValueX', 'AnnValueI',
                                                                         'cost_airpol']]]
 undisturbed_cost_low.index = undisturbed_cost_low.index.droplevel([0, 3])
 undisturbed_cost_low.columns = undisturbed_cost_low.columns.droplevel(0)
-undisturbed_cost_low['trade_balance'] = undisturbed_cost_low[['export_value', 'import_value']].sum(axis=1)
-undisturbed_cost_low['syscost_net'] = undisturbed_cost_low['syscost'] - undisturbed_cost_low['trade_balance']
+undisturbed_cost_low['trade_balance'] = undisturbed_cost_low[['AnnValueX', 'AnnValueI']].sum(axis=1)
+undisturbed_cost_low['syscost_net'] = undisturbed_cost_low['cost_zonal'] - undisturbed_cost_low['trade_balance']
 undisturbed_cost_low['total_cost'] = undisturbed_cost_low[['syscost_net', 'cost_airpol']].sum(axis=1)
 
-wind_add_low = results.loc[idx['base', :, :, 16715], idx['AT', 'add_wind_on']].unstack(1)
+wind_add_low = results.loc[idx['base', :, :, 16715], idx['AT', 'add_r_wind_on']].unstack(1)
 wind_add_low.index = wind_add_low.index.droplevel([0, 2])
 
 undisturbed_marginal_low = (undisturbed_cost_low['total_cost'].unstack(0).iloc[::-1].diff().divide(
@@ -249,7 +249,7 @@ plot_lines(undisturbed_marginal_low / 1000, os.path.join(FPATH, 'undisturbed_low
            xlabel='Added Capacity of Wind [GW]', ylabel='thousand € / MW', color=REFUEL_COLORS)
 
 # %% plot sensitivity of optimal res capacity deployment to capital cost of pv
-sensitivity = results.loc[idx['cheappv', :, 18, :], idx['AT', ['add_pv', 'add_wind_on']]].copy()
+sensitivity = results.loc[idx['pv_sens', :, 18, :], idx['AT', ['add_r_pv', 'add_r_wind_on']]].copy()
 sensitivity.index = sensitivity.index.droplevel([0, 2])
 sensitivity.columns = sensitivity.columns.droplevel(0)
 sensitivity = sensitivity.unstack(0)
