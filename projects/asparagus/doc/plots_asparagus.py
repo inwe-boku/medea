c3e10c552beace9589471b15d2b4b6b06468ad24
Sebastian Wehrle <seb.wehrle@gmail.com>
2020-05-13 07:07:42 +0200

    uodate of project asparagus

--- projects/asparagus/doc/plots_asparagus.py
+++ projects/asparagus/doc/plots_asparagus.py
@@ -1,15 +1,15 @@
-import itertools
+# %% imports
 import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
 import config as cfg
+from src.tools.visualize_data import plot_lines, plot_sublines, plot_subn
 
 # %% ----- ----- ----- ----- settings ----- ----- ----- -----
 idx = pd.IndexSlice
 PRICE_CO2 = 60
+# TODO: define color palette
 REFUEL_COLORS = ['#c72321', '#0d8085', '#f0c220', '#595959', '#3b68f9', '#7794dd']
 RES_COLORS = ['#d69602', '#ffd53d', '#3758ba', '#7794dd']
 RES_COLORS3 = ['#d69602', '#e5b710', '#ffd53d', '#3758ba', '#3b68f9', '#7794dd']
@@ -25,164 +25,43 @@ FPATH = os.path.join(cfg.MEDEA_ROOT_DIR, 'projects', 'asparagus', 'doc', 'figure
 if not os.path.exists(FPATH):
     os.makedirs(FPATH)
 
-
-# %% define plotting functions
-def plot_subn(df, fname, width=2, xlim=None, ylim=None, xlabel=None, ylabel=None, color=None):
-    """
-    plots each column of a pandas dataframe on a seperate subplot. Uses column names as subplot-titles.
-    :param df:
-    :param fname:
-    :param width:
-    :param xlim:
-    :param ylim:
-    :param xlabel:
-    :param ylabel:
-    :param color:
-    :return:
-    """
-    if isinstance(df, pd.DataFrame):
-        ncols = len(df.columns)
-    else:
-        raise TypeError
-
-    sub_length = np.ceil(ncols / width).astype(int)
-    sub_boxes = list(itertools.product(range(0, width), range(0, sub_length)))
-
-    figure, axis = plt.subplots(width, sub_length, figsize=(8, 5))
-    for col in range(0, ncols):
-        a = sub_boxes[col][0]
-        b = sub_boxes[col][1]
-
-        if color is not None:
-            axis[a, b].plot(df.iloc[:, col], linewidth=2, color=color[col])
-        else:
-            axis[a, b].plot(df.iloc[:, col], linewidth=2)
-
-        if xlabel is not None:
-            axis[a, b].set_xlabel(xlabel)
-
-        if (ylabel is not None) & (len(ylabel) == 1):
-            axis[a, b].set_ylabel(*ylabel)
-        elif (ylabel is not None) & (len(ylabel) == ncols):
-            axis[a, b].set_ylabel(ylabel[col])
-
-        if xlim is not None:
-            axis[a, b].set_xlim(xlim[0], xlim[1])
-
-        if ylim is not None:
-            axis[a, b].set_ylim(ylim[0], ylim[1])
-
-        axis[a, b].grid()
-        axis[a, b].set_title(df.columns[col])
-    # plt.rcParams.update({'font.size': 16})
-    plt.tight_layout()
-    plt.savefig(fname)
-    plt.close()
-
-
-def plot_lines(df, fname, xlim=None, ylim=None, xlabel=None, ylabel=None, color=None):
-    """
-    line plot of each column in df.
-    :param df:
-    :param fname:
-    :param xlim:
-    :param ylim:
-    :param xlabel:
-    :param ylabel:
-    :param color:
-    :return:
-    """
-    if isinstance(df, pd.DataFrame):
-        ncols = len(df.columns)
-    elif isinstance(df, pd.Series):
-        ncols = 1
-    else:
-        raise TypeError
-
-    figure, axis = plt.subplots(1, 1, figsize=(8, 5))
-    axis.set_ylabel(ylabel)
-    axis.set_xlabel(xlabel)
-
-    if isinstance(df, pd.DataFrame):
-        for col in range(0, ncols):
-            axis.plot(df.iloc[:, col], linewidth=2, color=color[col])
-    elif isinstance(df, pd.Series):
-        axis.plot(df, color=color[0])
-    else:
-        raise TypeError
-
-    if isinstance(df, pd.DataFrame):
-        axis.legend(df.columns)
-    elif isinstance(df, pd.Series):
-        axis.legend([df.name])
-    else:
-        raise TypeError
-
-    axis.set_ylim(ylim)
-    axis.set_xlim(xlim)
-    axis.grid()
-    # plt.rcParams.update({'font.size': 16})
-    plt.tight_layout()
-    plt.savefig(fname)
-    plt.close()
-
-
-def plot_sublines(df, fname, width=2, midx_level=0, subtitle=True, xlim=None, ylim=None, xlabel=None, ylabel=None,
-                  color=None):
-    """
-    plots multiindex pd.DataFrame to multiple line-subplots
-    :param df: Pandas MultiIndex DataFrame
-    :param fname: name of output file generated
-    :param width: number of subplots in a horizontal row
-    :param midx_level: MultiIndex level to group plots by
-    :param subtitle: boolean, if True generate title for each subplot; False: generate long legend entries instead
-    :param xlim:
-    :param ylim:
-    :param xlabel:
-    :param ylabel:
-    :param color:
-    :return: file with subplots
-    """
-    # determine length and width of subfigures
-    idx_to_group = list(df.columns.get_level_values(midx_level).unique())
-    num_subfigures = len(idx_to_group)
-    sub_length = np.ceil(num_subfigures / width).astype(int)
-
-    fig = plt.figure(figsize=(8, 5))
-    for subplot in range(0, num_subfigures):
-        data_to_plot = df.xs(idx_to_group[subplot], axis=1, level=midx_level)
-        _, num_lines = data_to_plot.shape
-        ax = fig.add_subplot(width, sub_length, subplot + 1)
-
-        if not color:
-            ax.plot(data_to_plot, linewidth=2)
-        elif len(color) == num_lines:
-            for c in range(0, num_lines):
-                ax.plot(data_to_plot.iloc[:, c], linewidth=2, color=color[c])
-        else:
-            raise TypeError(f'Colors misspecified. Should be {num_lines} colors')
-
-        if subtitle:
-            ax.set_title(idx_to_group[subplot])
-            ax.legend(data_to_plot.columns)
-        if not subtitle:
-            legend_entries = [", ".join(i) for i in itertools.product(list(data_to_plot.columns),
-                                                                      [idx_to_group[subplot]])]
-            ax.legend(legend_entries)
-        ax.set_ylabel(ylabel)
-        ax.set_xlabel(xlabel)
-        ax.set_ylim(ylim)
-        ax.set_xlim(xlim)
-        ax.grid()
-
-    plt.tight_layout()
-    plt.savefig(fname)
-    plt.close()
-
-
 # %% ----- ----- ----- ----- read results ----- ----- ----- -----
 results = pd.read_csv(RPATH, decimal=',', delimiter=';', index_col=[0, 1, 2, 3, 4], header=[0])
 results = results.unstack(-1)
+
+# %% analyze system configuration (capacities added and decommissioned)
+cap_init_at = {
+    'ng_stm': 0.13932,
+    'ng_stm_chp': 0.606472,
+    'ng_cbt_lo': 0.1282346,
+    'ng_cbt_lo_chp': 0.27724938,
+    'ng_cc_lo': 0.15136,
+    'ng_cc_lo_chp': 0.32938,
+    'ng_cc_hi_chp': 2.286138,
+    'oil_stm': 0.08584,
+    'oil_cbt': 0.003915,
+    'oil_cbt_chp': 0.04756,
+    'oil_cc': 0.0348,
+    'bio': 0.354965,
+    'bio_chp': 0.369499,
+    'ng_boiler_chp': 3.87,
+    'heatpump_pth': 0.1
+}
+
+capi = pd.DataFrame.from_dict(cap_init_at, orient='index', columns=['base'])
+capi.columns.name = 'scenario'
+capi.index.name = 'variable'
+
+capa = results.loc[idx['base', :, :, 36715], idx['AT', :]]
+capa.columns = capa.columns.droplevel(0)
+capa = capa.loc[:, (capa.columns.str.contains('add_g') | capa.columns.str.contains('deco'))]
+capa.columns = pd.MultiIndex.from_tuples(list(capa.columns.str.split('_g_')))
+capa = capa.groupby(level=1, axis=1).sum()
+capa.columns.name = 'variable'
+capa.index = capa.index.rename(['scenario', 'co2price', 'wind_lim', 'pv_cost'])
+capa = (capa + capi.T).dropna(axis=1, how='all')
+
+# plot_lines(capa, os.path.join(FPATH, 'capacity.pdf'),color=REFUEL_COLORS)
 
 # %% ----- ----- ----- plot wind restriction: changes in system operation ----- ----- -----
 # net electricity generation, net electricity exports, fossil thermal generation, CO2 emissions from energy generation
