8ca97b66c345d70348a2dc1c704d7adacc145fed
Sebastian <sebastian.wehrle@boku.ac.at>
2022-02-04 17:16:25 +0100

    allow for existing project directories

--- medea/init.py
+++ medea/init.py
@@ -14,17 +14,17 @@ def medea_init(root_dir):
     root_dir = Path(root_dir)
     if not os.path.exists(root_dir):
         os.makedirs(root_dir)
-        os.makedirs(root_dir / 'data')
-        os.makedirs(root_dir / 'data' / 'results')
-        os.makedirs(root_dir / 'doc')
-        os.makedirs(root_dir / 'doc' / 'figures')
-        os.makedirs(root_dir / 'opt')
-        os.makedirs(root_dir / 'src')
-        with open(root_dir / 'config.py', 'x') as cfg:
-            cfg.write(f'ROOT_DIR = {root_dir}\n')
 
-        # fetch main gams model
-        package_dir = Path(sysconfig.get_path('data'))
-        copyfile(package_dir / 'gms' / 'medea_main.gms', root_dir / 'opt' / 'medea_main.gms')
-    else:
-        raise ValueError('Directory already exists')
+    os.makedirs(root_dir / 'data')
+    os.makedirs(root_dir / 'data' / 'results')
+    os.makedirs(root_dir / 'doc')
+    os.makedirs(root_dir / 'doc' / 'figures')
+    os.makedirs(root_dir / 'opt')
+    os.makedirs(root_dir / 'src')
+    with open(root_dir / 'config.py', 'x') as cfg:
+        cfg.write(f'ROOT_DIR = {root_dir}\n')
+
+    # fetch main gams model
+    package_dir = Path(sysconfig.get_path('data'))
+    copyfile(package_dir / 'gms' / 'medea_main.gms', root_dir / 'opt' / 'medea_main.gms')
+    copyfile(package_dir / 'gms' / 'medea_custom.gms', root_dir / 'opt' / 'medea_custom.gms')
